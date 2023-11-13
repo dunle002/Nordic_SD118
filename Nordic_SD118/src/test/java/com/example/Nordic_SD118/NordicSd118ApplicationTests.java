7d84bcf9d7b28d1642cde1d5c0de70daa2d794dd
Lê Xuân Dương <17102002cc@gmail.com>
2023-11-13 20:40:32 +0700

    Initial commit

---
+++ Nordic_SD118/src/test/java/com/example/Nordic_SD118/NordicSd118ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Nordic_SD118;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NordicSd118ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
